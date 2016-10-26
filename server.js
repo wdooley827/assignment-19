e25cfcaf67cbe1fcde7c342f0c7c5d3165f9d2db
Travis Hubbard <t3patterson@gmail.com>
2016-10-26 07:53:21 -0400

    updating w/ favicon, pruning body parser

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-const bodyParser = require('body-parser')
 const express = require('express')
 const renderFile = require('ejs').renderFile
 const app = express()
 
-const theRoot = __dirname + '/dist/',
-	PORT = process.env.PORT || 3000 
+const theRootDir = __dirname + '/dist/'
+const PORT = process.env.PORT || 3000
 
 // got env port for heroku or elsewhere, else set to 3000 for dev
 app.set('port', PORT)
 
-app.set('views', './dist/views');
+app.set('views', './public/views');
 app.engine('html', renderFile)
 app.set('view engine', 'html');
 
-// serving images from dist/assets/
-app.use( express.static( __dirname + '/dist/assets') );
-
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded() );
-
-app.post('/auth/register', function(req, res){
-  console.log(req.body)
-  res.redirect('/')
-})
+// serving all files from dist/assets/
+app.use( express.static( __dirname + '/public/assets') );
 
 // get the root page
 app.get('/', function (req, res) {
@@ -30,12 +21,9 @@ app.get('/', function (req, res) {
 });
 
 //get the register page
-app.get('/register', function (req, res) {
-  res.render('register');
+app.get('/about', function (req, res) {
+  res.render('about-page');
 });
-
-
-
 
 app.listen(PORT,function() {
 	console.log('\n\n===== listening for requests on port ' + PORT + ' =====\n\n')
