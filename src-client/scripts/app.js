d7506d76649a991421e6d6b6df15afa524d213bd
Travis Hubbard <t3patterson@gmail.com>
2016-11-10 09:41:45 -0500

    setting up the watchers

--- src-client/scripts/app.js
+++ src-client/scripts/app.js
@@ -2,6 +2,6 @@ const ReactDOM = require('react-dom');
 const React = require('react')
 const Backbone = require('backbone');
 
-document.querySelector('#app-container').innerHTML = `<h1>YOLO</h1>`
+document.querySelector('#app-container').innerHTML = `<h1>Yah okay</h1>`
 
 new AppRouter()
